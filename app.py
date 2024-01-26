9f351c0f335a2a264d29a9c3df6d7a7de6fab7f3
ShivamY-30 <113720988+ShivamY-30@users.noreply.github.com>
2024-01-26 11:26:01 +0530

    Sixth commit

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ import streamlit as st
 from streamlit.components.v1 import iframe
 from streamlit_option_menu import option_menu
 
-#CUSTOM CSS
+# CUSTOM CSS
 hide_btn = """
          <style> 
-    
-         .st-emotion-cache-6q9sum, .ef3psqc4 {visibility : hidden;}
+         background-color: hsl(222.2, 84%, 4.9%);
+         margin-top:-50px;
+         # .st-emotion-cache-6q9sum, .ef3psqc4 {visibility : hidden;}
          .st-emotion-cache-1wbqy5l, .e17vllj40 {visibility : hidden;}
          
          </style>
 
 """
 
-st.markdown(hide_btn , unsafe_allow_html=True)
+st.markdown(hide_btn, unsafe_allow_html=True)
+
+
 def about_us():
     st.title("Welcome to our innovative job recommendation algorithm!")
     st.divider()
@@ -26,26 +29,24 @@ def about_us():
              "we provide tailored recommendations, ensuring you explore opportunities that match your capabilities. "
              "Unlock a world of career possibilities with our cutting-edge algorithm, guiding you towards fulfilling "
              "and rewarding professional paths. Elevate your career journey with precision and personalized recommendations.")
+    st.write("Contact us: yadavshivamp90671@gmail.com")
     st.divider()
     st.divider()
 
-st.sidebar.image("logo.png",width=150, use_column_width=False)
+
+# st.sidebar.image("logo.png",width=150, use_column_width=False)
 
 with st.sidebar:
-       selected = st.selectbox(
-            "Navigate",
-            ["Home", "About Us"]
-        )
-
+    selected = st.selectbox(
+        "",
+        ["Home", "About Us"]
+    )
 
 if selected == "About Us":
     about_us()
 
 
-
-
-
-#FUNCTIONS SECTION
+# FUNCTIONS SECTION
 
 
 def find_des(title):
@@ -53,8 +54,6 @@ def find_des(title):
     print(job_index)
     text = jobs.jobdescription[job_index]
     return text
-
-
 
 
 def recommend(skill):
@@ -77,16 +76,15 @@ def recommend(skill):
 
 # TITLE SECTION
 
-st.header('Job recommendation system')
+st.write('Job recommendation system')
 st.divider()
 
-#IMPORTING FILES
+# IMPORTING FILES
 job_dict = pickle.load(open('job_recommend.pkl', 'rb'))
 jobs = pd.DataFrame(job_dict)
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-
-#SELECTBOX SECTION
+# SELECTBOX SECTION
 Selected_skill = st.selectbox(
     'Enter or  Select Your skill',
     jobs['skills'].values)
@@ -100,7 +98,7 @@ if st.button('Recommend?', type="primary"):
         recomendations = recommend(Selected_skill)
         num_elements = len(recomendations)
 
-            # Calculate the number of columns per row (you can adjust this based on your preference)
+        # Calculate the number of columns per row (you can adjust this based on your preference)
         columns_per_row = 2
 
         # Calculate the number of rows
@@ -111,26 +109,27 @@ if st.button('Recommend?', type="primary"):
 
         # Iterate through recommendations and display in dynamically generated rows and columns
         element_counter = 0
-        n = 20
+        n = 30
         for row in rows:
             for col in row:
                 if element_counter < num_elements:
                     text = find_des(recomendations[element_counter])
-                    x =  str(text)
+                    x = str(text)
                     type(x)
                     result_list = x.split()[:n]
                     result_str = " ".join(result_list)
-                     # input_str = "Python is a programming language and we can develop applications using it"
-
+                    # input_str = "Python is a programming language and we can develop applications using it"
 
                     col.write(
 
-                        f'<div style="border: 2px solid red; padding:10px 10px; margin: 5px; border-radius:8px">Job Title/Position : {recomendations[element_counter]} <br>Job Description : {result_str}...</div>',
+
+                        f'<div style="height: 160px; margin-bottom:20px; overflow:scroll;  width: 100%; box-shadow:2px 2px 3px grey; padding:10px 10px; margin: 5px; border-radius:8px";>Job Title/Position : {recomendations[element_counter]} <br>Job Description : {result_str}...</div>',
                         unsafe_allow_html=True
                     )
                     element_counter += 1
     except IndexError:
-        st.write("⚠️ Sorry At current time we are not having enough Information regarding your inputs, to suggest you your desired output! Please try again later ")
+        st.write(
+            "⚠️ Sorry At current time we are not having enough Information regarding your inputs, to suggest you your desired output! Please try again later ")
 
     # # Calculate the number of columns per row (you can adjust this based on your preference)
     # columns_per_row = 2
@@ -162,25 +161,23 @@ if st.button('Recommend?', type="primary"):
     #             )
     #             element_counter += 1
     #
-
-# Footer
-footer_html = """
-    <style>
-        .footer {
-            margin-top:50px;
-            background-color:  rgb(14, 17, 23);
-            color:yellow;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
-
-    <div class="footer">
-        <p>© EduSync 2024. All rights reserved.</p>
-        <p>Contact us: yadavshivamp90671@gmail.com</p>
-    </div>
-"""
-
-st.markdown(footer_html, unsafe_allow_html=True)
-
-
+#
+# # Footer
+# footer_html = """
+#     <style>
+#         .footer {
+#             margin-top:50px;
+#             background-color:  rgb(14, 17, 23);
+#             color:yellow;
+#             padding: 10px;
+#             text-align: center;
+#         }
+#     </style>
+#
+#     <div class="footer">
+#         <p>© EduSync 2024. All rights reserved.</p>
+#         <p>Contact us: yadavshivamp90671@gmail.com</p>
+#     </div>
+# """
+#
+# st.markdown(footer_html, unsafe_allow_html=True)
